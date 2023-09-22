77bebc521ce76ce3b52c1ad5e385772212adabb9
Saimun Hassan <saimun4u@gmail.com>
2023-09-22 00:11:34 -0400

    Added error message display for unknown input words

--- app.js
+++ app.js
@@ -6,20 +6,28 @@ const meaningEl = document.getElementById('meaning');
 const audioEl = document.getElementById('audio');
 
 async function fetchAPI(word) {
-  infoTextEl.innerText = `Searching for the meaning of the word "${word}"`;
+  infoTextEl.innerText = `Searching for meaning of the word "${word}"`;
 
   try {
     infoTextEl.style.display = 'block';
     meaningContainerEl.style.display = 'none';
     const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
     const response = await fetch(url).then((res) => res.json());
-    console.log(response);
-    infoTextEl.style.display = 'none';
-    meaningContainerEl.style.display = 'block';
-    titleEl.textContent = response[0].word;
-    meaningEl.textContent =
-      response[0].meanings[0].definitions[0]['definition'];
-    audioEl.src = response[0].phonetics[0].audio;
+    if (response.title) {
+      infoTextEl.style.display = 'none';
+      meaningContainerEl.style.display = 'block';
+      titleEl.textContent = word;
+      meaningEl.textContent = 'Meaning is not available';
+      audioEl.style.display = none;
+    } else {
+      infoTextEl.style.display = 'none';
+      meaningContainerEl.style.display = 'block';
+      audioEl.style.display = 'inline-flex';
+      titleEl.textContent = response[0].word;
+      meaningEl.textContent =
+        response[0].meanings[0].definitions[0]['definition'];
+      audioEl.src = response[0].phonetics[0].audio;
+    }
   } catch (error) {
     console.log['title'];
   }
