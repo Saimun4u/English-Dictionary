a92b7b79f494af7a36b2cee3aa6f5a44b9e6e7ab
Saimun Hassan <saimun4u@gmail.com>
2023-09-21 23:56:28 -0400

    Added audio pronunciation of word entered

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const infoTextEl = document.getElementById('info-text');
 const meaningContainerEl = document.getElementById('meaning-container');
 const titleEl = document.getElementById('title');
 const meaningEl = document.getElementById('meaning');
+const audioEl = document.getElementById('audio');
 
 async function fetchAPI(word) {
   infoTextEl.innerText = `Searching for the meaning of the word "${word}"`;
@@ -12,13 +13,15 @@ async function fetchAPI(word) {
     meaningContainerEl.style.display = 'none';
     const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
     const response = await fetch(url).then((res) => res.json());
+    console.log(response);
     infoTextEl.style.display = 'none';
     meaningContainerEl.style.display = 'block';
     titleEl.textContent = response[0].word;
     meaningEl.textContent =
       response[0].meanings[0].definitions[0]['definition'];
+    audioEl.src = response[0].phonetics[0].audio;
   } catch (error) {
-    console.log(error);
+    console.log['title'];
   }
 }
 
