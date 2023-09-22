d306fb039e157be2da315ed618abfc7b114a1244
Saimun Hassan <saimun4u@gmail.com>
2023-09-21 21:28:12 -0400

    Added display prop. transition showing word is being searched

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const inputEl = document.getElementById('input');
+const infoTextEl = document.getElementById('info-text');
 
 async function fetchAPI(word) {
-  const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
-  const response = await fetch(url).then((res) => res.json());
-  console.log(response);
+  infoTextEl.innerText = `Searching for the meaning of the word "${word}"`;
+
+  try {
+    infoTextEl.style.display = 'block';
+
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+    const response = await fetch(url).then((res) => res.json());
+    infoTextEl.style.display = 'none';
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 inputEl.addEventListener('keyup', (e) => {
