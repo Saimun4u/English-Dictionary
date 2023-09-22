f9db27d4561107dfc1c84926caa3efab5857a91b
Saimun Hassan <saimun4u@gmail.com>
2023-09-21 21:22:59 -0400

    Use fetchAPI to grab meaning of word for function

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const inputEl = document.getElementById('input');
 
+async function fetchAPI(word) {
+  const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+  const response = await fetch(url).then((res) => res.json());
+  console.log(response);
+}
+
 inputEl.addEventListener('keyup', (e) => {
-  console.log(e.target.value);
+  if (e.target.value && e.key === 'Enter') fetchAPI(e.target.value);
 });
