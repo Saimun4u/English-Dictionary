bfdb7ff24be8c5e1362ef939c25574393f44689b
Saimun Hassan <saimun4u@gmail.com>
2023-09-21 23:43:39 -0400

    Added display of word and meaning

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const inputEl = document.getElementById('input');
 const infoTextEl = document.getElementById('info-text');
+const meaningContainerEl = document.getElementById('meaning-container');
+const titleEl = document.getElementById('title');
+const meaningEl = document.getElementById('meaning');
 
 async function fetchAPI(word) {
   infoTextEl.innerText = `Searching for the meaning of the word "${word}"`;
 
   try {
     infoTextEl.style.display = 'block';
-
+    meaningContainerEl.style.display = 'none';
     const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
     const response = await fetch(url).then((res) => res.json());
     infoTextEl.style.display = 'none';
-    console.log(response);
+    meaningContainerEl.style.display = 'block';
+    titleEl.textContent = response[0].word;
+    meaningEl.textContent =
+      response[0].meanings[0].definitions[0]['definition'];
   } catch (error) {
     console.log(error);
   }
